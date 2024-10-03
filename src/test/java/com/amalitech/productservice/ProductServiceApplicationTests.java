b440aec9b1f5c9b5a15c3fde56bddbace3f07c10
nccharles <nccharles1@gmail.com>
2024-10-03 15:17:55 +0200

    Finalized services setup

---
+++ src/test/java/com/amalitech/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalitech.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
